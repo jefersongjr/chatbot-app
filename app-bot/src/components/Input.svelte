0d74fb966a01076bb2a0f6bab435ec51720b93d6
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-10-16 11:11:03 -0300

    feat :sparkles: criando componente de input

---
+++ app-bot/src/components/Input.svelte
@@ -0,0 +1,23 @@
+<script lang="ts"> 
+  export let label = '';
+  export let type = 'text';
+  export let value = '';
+  export let placeholder = '';
+  export let onInput;
+
+  const handleInput = (event) => {
+    if(onInput) {
+        onInput(event.target.value)
+    }
+  }
+</script>
+
+<div>
+    <label>{label}</label>
+    <input
+      type={type}
+      value={value}
+      placeholder={placeholder}
+      on:input={handleInput}
+    />
+</div>
